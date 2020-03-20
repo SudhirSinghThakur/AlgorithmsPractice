ae10b28a954a56d11a955e90f5e9ae3743166a01
Sudhir Singh Thakur <sudhirthakur4@gmail.com>
2020-03-20 13:38:28 +0530

    Added first basic algo implementation.

---
+++ BasicAlgorithms/Palindrome.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BasicAlgorithms
+{
+    /// <summary>
+    /// Program
+    /// </summary>
+    class Palindrome
+    {
+        /// <summary>
+        /// Main method.
+        /// Starting point of the program.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the input string : ");
+            string input = Console.ReadLine();
+            Console.WriteLine("IsPalindrome : {0}", IsPalindrome(input));
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Program to find the given string is palindrome or not.
+        /// The bestcase runtime of this method will be logN.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static bool IsPalindrome(string input)
+        {
+            for (int index = 0; index < input.Length / 2; index++)
+            {
+                if (input[index] != input[(input.Length - 1) - index])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
